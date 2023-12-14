f38b48c70bdec88486a5294e9d8edc2248cbd55f
DryRun03 <pankajgarg.garg03@gmail.com>
2023-12-14 13:35:15 +0530

    Adding new file for routes

--- server.js
+++ server.js
@@ -1,90 +1,24 @@
-require('dotenv').config()
-const express= require('express')
-const mongoose = require('mongoose')
-const app = express()
-const Books = require('./models/booksModel')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bookRoute = require('./routes/BooksRoute');
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-//routes
 
-
-//Add Books to the DB
-app.post('/books',async(req,res)=>{
-    try {
-        const books = await Books.create(req.body);
-        res.status(200).json(books);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
-
-//Get All books
-app.get('/books',async(req,res)=>{
-    try {
-        const books = await Books.find({});
-        res.status(200).json(books);
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-})
-
-//Get Books By Id
-app.get('/books/:id',async(req,res)=>{
-    try {
-        const {id}=req.params;
-        const books = await Books.findOne({ id });
-        res.status(200).json(books);
-    } catch (error) {
-        res.status(404).json("Book with this id not found");
-    }
-})
-
-
-// Update/edit Books by Id
-app.put('/books/:id',async(req,res)=>{
-    try {
-        const {id} = req.params;
-        const updatedBook = await Books.findOneAndUpdate(
-            { id },
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (updatedBook) {
-            res.status(200).json(updatedBook);
-        } else {
-            res.status(404).json({ message: 'Book not found with this id' });
-        }
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-})
-
-
-//Delete Books API by Id
-app.delete('/book/:id',async(req,res)=>{
-    try {
-        const {id} = req.params;
-        const book = await Books.findOneAndDelete({id});
-        if(book){
-            res.status(200).json(book);
-        }else{
-            res.status(404).json("Book not found with this id");
-        }
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-})
+// Routes
+app.use('/api', bookRoute);
 
 mongoose.connect(MONGO_URL)
-.then(()=>{
-    app.listen(PORT,()=>{
-        console.log('Node API app is running on port ${PORT}')
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on port ${PORT}`);
     });
-    console.log('Connected to MongoDB')
-}).catch((error)=>{
-    console.log(error)
-})
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
